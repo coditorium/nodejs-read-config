95c4e0e800d7744d26cbe32a7fbf7f510e8a1bd0
Paweł Mendelski <pawel.mendelski@rulefinancial.com>
2015-03-11 09:46:48 +0100

    Enabled namespaces in properties format

--- test/load/parse/properties/parse.unit.js
+++ test/load/parse/properties/parse.unit.js
@@ -6,14 +6,22 @@ var expect = require('chai').expect,
 describe("properties.parse module test:", function() {
 
 	var validProperties = { x: 'a' },
-		validPropertiesText = "x = a";
+		validPropertiesText = "x = a",
+		validNestedProperties = { x: { y: 'a' } },
+		validNestedPropertiesText = "x.y = a";
 
 	describe("should parseSync properties document", function() {
 
-		it("and return object", function() {
+		it("and return an object", function() {
 			var result = properties.parseSync(validPropertiesText);
 			expect(result).to.exist;
 			expect(result).to.be.eql(validProperties);
+		});
+
+		it("and return a nested object", function() {
+			var result = properties.parseSync(validNestedPropertiesText);
+			expect(result).to.exist;
+			expect(result).to.be.eql(validNestedProperties);
 		});
 
 		it("and return empty object on empty json", function() {
@@ -31,6 +39,15 @@ describe("properties.parse module test:", function() {
 				if (err) return done(err);
 				expect(result).to.exist;
 				expect(result).to.be.eql(validProperties);
+				done();
+			});
+		});
+
+		it("and return a nested object", function(done) {
+			properties.parse(validNestedPropertiesText, function(err, result) {
+				if (err) return done(err);
+				expect(result).to.exist;
+				expect(result).to.be.eql(validNestedProperties);
 				done();
 			});
 		});
