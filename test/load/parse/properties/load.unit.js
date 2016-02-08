13b9ebcef56ebcc4139c626beca797153b34d943
mendlik <mendelski.pawel@gmail.com>
2016-02-08 08:19:08 +0100

    Migrating to es6

--- test/load/parse/properties/load.unit.js
+++ test/load/parse/properties/load.unit.js
@@ -1,51 +1,51 @@
 'use strict';
 
-var expect = require('chai').expect,
-	path = require('path'),
-	properties = requireLib('load/parse/properties');
+const expect = require('chai').expect,
+    path = require('path'),
+    properties = requireLib('load/parse/properties');
 
-describe("properties.load test:", function() {
+describe('properties.load test:', () => {
 
-	var validPropertiesFile = path.resolve(__dirname, './valid.properties'),
-		validProperties = { x: 'a' },
-		emptyPropertiesFile = path.resolve(__dirname, './empty.properties');
+    const validPropertiesFile = path.resolve(__dirname, './valid.properties'),
+        validProperties = { x: 'a' },
+        emptyPropertiesFile = path.resolve(__dirname, './empty.properties');
 
-	describe("should loadSync properties document", function() {
+    describe('should loadSync properties document', () => {
 
-		it("and return object", function() {
-			var result = properties.loadSync(validPropertiesFile);
-			expect(result).to.exist;
-			expect(result).to.have.property('x', 'a');
-		});
+        it('and return object', () => {
+            const result = properties.loadSync(validPropertiesFile);
+            expect(result).to.exist;
+            expect(result).to.have.property('x', 'a');
+        });
 
-		it("and return empty object", function() {
-			var result = properties.loadSync(emptyPropertiesFile);
-			expect(result).to.exist;
-			expect(result).to.be.eql({});
-		});
+        it('and return empty object', () => {
+            const result = properties.loadSync(emptyPropertiesFile);
+            expect(result).to.exist;
+            expect(result).to.be.eql({});
+        });
 
-	});
+    });
 
-	describe("should loadAsync properties document", function() {
+    describe('should loadAsync properties document', () => {
 
-		it("and return object", function(done) {
-			properties.load(validPropertiesFile, function(err, result) {
-				if (err) return done(err);
-				expect(result).to.exist;
-				expect(result).to.be.eql(validProperties);
-				done();
-			});
-		});
+        it('and return object', (done) => {
+            properties.load(validPropertiesFile, (err, result) => {
+                if (err) return done(err);
+                expect(result).to.exist;
+                expect(result).to.be.eql(validProperties);
+                done();
+            });
+        });
 
-		it("and return empty object", function(done) {
-			properties.load(emptyPropertiesFile, function(err, result) {
-				if (err) return done(err);
-				expect(result).to.exist;
-				expect(result).to.be.eql({});
-				done();
-			});
-		});
+        it('and return empty object', (done) => {
+            properties.load(emptyPropertiesFile, (err, result) => {
+                if (err) return done(err);
+                expect(result).to.exist;
+                expect(result).to.be.eql({});
+                done();
+            });
+        });
 
-	});
+    });
 
 });
